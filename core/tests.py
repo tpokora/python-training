4313dd42ce3a713ed99065c3820c573e969ed4ef
tpokora <pokora.tomek@gmail.com>
2019-12-12 21:43:53 +0100

    #3 Home Page Basic test

--- core/tests.py
+++ core/tests.py
@@ -2,9 +2,6 @@ from django.test import TestCase, Client
 
 
 # Create your tests here.
-from django.test.utils import setup_test_environment
-
-
 class CoreHomeTests(TestCase):
 
     def test_home(self):
@@ -15,4 +12,3 @@ class CoreHomeTests(TestCase):
         text = "<h1>Hello World</h1>"
         self.assertEqual(response.status_code, 200)
         self.assertEqual(text in content, True)
-
