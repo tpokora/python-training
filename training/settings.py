39f70a92863e6da7f8924c961a6438f803d1f24d
Tomasz Pokora <pokora.tomek@gmail.com>
2019-12-15 12:51:22 +0100

    Use heroku db config

--- training/settings.py
+++ training/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -82,6 +83,8 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+DATABASES['default'] = dj_database_url.config()
 
 
 # Password validation
