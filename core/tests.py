0f86af63796aa9b8ee98ebcbd66b87a364fc1993
tpokora <pokora.tomek@gmail.com>
2019-12-22 17:37:49 +0100

    Closes #4 Update UserDetails and tests

--- core/tests.py
+++ core/tests.py
@@ -8,6 +8,11 @@ from django.urls import reverse
 # Create your tests here.
 class CoreHomeTests(TestCase):
 
+    H2_TAG = '<h2>'
+    H2_TAG_CLOSE = '</h2>'
+    H3_TAG = '<h3>'
+    H3_TAG_CLOSE = '</h3>'
+
     def test_home(self):
         client = Client()
 
@@ -39,7 +44,8 @@ class CoreHomeTests(TestCase):
 
         response = client.get(reverse("core:user", args=(user.id,)))
         content = str(response.content)
-        header = "<h2>User"
+        user_header = "%s%s%s" % (self.H2_TAG, user.username, self.H2_TAG_CLOSE)
+        user_email = "%sEmail: %s%s" % (self.H3_TAG, user.email, self.H3_TAG_CLOSE)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(header in content, True)
-        self.assertEqual(user.username in content, True)
+        self.assertEqual(user_header in content, True,  "%s is not having %s" % (user_header, user.username))
+        self.assertEqual(user_email in content, True, "%s is not having %s" % (user_email, user.email))
