b298126d9b2c48bef1bbb9fe22f059319ed1cccc
tpokora <pokora.tomek@gmail.com>
2020-01-04 10:30:33 +0100

    Change admin console url

--- training/urls.py
+++ training/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('console/', admin.site.urls),
     path('', include('core.urls')),
     url(r'^accounts/', include('allauth.urls')),
 ]
