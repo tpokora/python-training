978ccffd68cb479a34b4fb7560bf2892bf0b66d6
tpokora <pokora.tomek@gmail.com>
2020-01-19 18:46:26 +0100

    #14 Add due notification on navbar

--- core/views.py
+++ core/views.py
@@ -3,10 +3,18 @@ from django.shortcuts import redirect
 from django.views.generic import TemplateView, ListView, DetailView
 
 from core.models import User
+from notes.models import NoteQuerySet
 
 
 class IndexView(TemplateView):
     template_name = 'home/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if not self.request.user.is_authenticated:
+            return context
+        context['notes_due_quantity'] = len(NoteQuerySet.get_user_past_due_notes(self.request.user).all())
+        return context
 
 
 class UsersListView(ListView):
