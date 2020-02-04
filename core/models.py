ebb26b0dbd4779b647c65791ad22db35f682fc44
tpokora <pokora.tomek@gmail.com>
2020-02-04 19:22:25 +0100

    #19 Add visible attribute for UserConfiguration

--- core/models.py
+++ core/models.py
@@ -1,3 +1,4 @@
+from django.contrib.postgres.fields import JSONField
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
@@ -7,12 +8,25 @@ class User(AbstractUser):
     def __str__(self):
         return "id: {}, username: {}, email: {}".format(self.pk, self.username, self.email)
 
+    def create_user_configuration(self):
+        configuration = UserConfiguration.objects.filter(user__id=self.pk)
+        if configuration.count() == 0:
+            configuration = UserConfiguration()
+            configuration.user = self
+            configuration.save()
+
+
+class Configuration:
+    DEFAULT_CONFIGURATION = dict({'visible': True})
+
 
 class UserConfiguration(models.Model):
-    user = models.ForeignKey(
+    user = models.OneToOneField(
         settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
+        on_delete=models.CASCADE
     )
+    configuration = JSONField(null=False, blank=False, default=Configuration.DEFAULT_CONFIGURATION)
 
     def __str__(self):
-        return "user: '{}'".format(self.user.__str__())
+        return "user: '{}', configuration: {}".format(self.user.__str__(), self.configuration)
+
--- core/tests.py
+++ core/tests.py
@@ -4,7 +4,7 @@ from django.test import Client
 from django.urls import reverse
 
 # Create your tests here.
-from core.models import User, UserConfiguration
+from core.models import User, UserConfiguration, Configuration
 
 
 class CoreTests(TestCase):
@@ -19,7 +19,14 @@ class CoreTests(TestCase):
             user.email = 'testUser@email.com'
             user.save()
             user = User.objects.filter(username='testUser')
+
         return user.get()
+
+    @staticmethod
+    def create_configuration_for_user(user):
+        user.create_user_configuration()
+        configuration = UserConfiguration.objects.filter(user__id=user.id)
+        return configuration.get()
 
     def test_user__str__(self):
         user = CoreTests.create_test_user()
@@ -31,79 +38,91 @@ class CoreTests(TestCase):
         user = CoreTests.create_test_user()
         configuration = UserConfiguration()
         configuration.user = user
+        configuration.configuration = Configuration.DEFAULT_CONFIGURATION
 
-        expected_conf_str = "user: '{}'".format(configuration.user.__str__())
+        expected_conf_str = "user: '{}', configuration: {}".format(configuration.user.__str__(),
+                                                                   configuration.configuration)
 
         self.assertEqual(configuration.__str__(), expected_conf_str, True)
 
 
-class CoreViewTests(TestCase):
-    H1_TAG = '<h1>'
-    H1_TAG_CLOSE = '</h1>'
-    H2_TAG = '<h2>'
-    H2_TAG_CLOSE = '</h2>'
-    H3_TAG = '<h3>'
-    H3_TAG_CLOSE = '</h3>'
-    P_TAG = '<p>'
-    P_TAG_CLOSE = '</p>'
+class UserConfigurationTests(TestCase):
 
-    def test_home_user_not_authorized(self):
-        client = Client()
-
-        login = "Login</a>"
-        response = client.get(reverse("core:index"))
-        content = str(response.content)
-        welcome = "{}Welcome!{}".format(self.H1_TAG, self.H1_TAG_CLOSE)
-        text = "{}Please login to view content{}".format(self.H2_TAG, self.H2_TAG_CLOSE)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(welcome in content, True)
-        self.assertEqual(text in content, True)
-        self.assertEqual(login in content, True)
-
-    def test_home_user_is_authorized(self):
-        client = Client()
-
+    def test_create_configuration_for_user(self):
         user = CoreTests.create_test_user()
-        client.force_login(user=user)
-        response = client.get(reverse("core:index"))
-        content = str(response.content)
-        welcome = "{}Welcome, {}!{}".format(self.H1_TAG, user.username, self.H1_TAG_CLOSE)
-        text = 'href="/user/%s"' % user.id
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(welcome in content, True)
-        self.assertEqual(text in content, True)
-        logged_as = "Logged as: %s" % user.username
-        self.assertEqual(logged_as in content, True)
-        logout = "Logout"
-        self.assertEqual(logout in content, True)
+        user_configuration = CoreTests.create_configuration_for_user(user)
 
-    def test_users(self):
-        client = Client()
+        self.assertEqual(user_configuration.user.pk, user.pk, True)
+        self.assertEqual(user_configuration.configuration, Configuration.DEFAULT_CONFIGURATION, True)
 
-        users = User.objects.all()
-        response = client.get(reverse("core:users"))
-        content = str(response.content)
-        header = "{}Users{}".format(self.H2_TAG, self.H2_TAG_CLOSE)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(header in content, True)
-        for user in users:
-            self.assertEqual(user.username in content, True)
 
-    def test_user_pk1(self):
-        client = Client()
-
-        user = CoreTests.create_test_user()
-        client.force_login(user=user)
-        response = client.get(reverse("core:user", args=(user.id,)))
-        content = str(response.content)
-        user_header = "%s%s%s" % (self.H2_TAG, user.username, self.H2_TAG_CLOSE)
-        user_email = "%sEmail: %s%s" % (self.P_TAG, user.email, self.P_TAG_CLOSE)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(user_header in content, True, "%s is not having %s" % (user_header, user.username))
-        self.assertEqual(user_email in content, True, "%s is not having %s" % (user_email, user.email))
-
-    def test_user_pk_not_authorized(self):
-        client = Client()
-
-        response = client.get(reverse("core:user", args=(1,)))
-        self.assertEqual(response.status_code, 302)
+# class CoreViewTests(TestCase):
+#     H1_TAG = '<h1>'
+#     H1_TAG_CLOSE = '</h1>'
+#     H2_TAG = '<h2>'
+#     H2_TAG_CLOSE = '</h2>'
+#     H3_TAG = '<h3>'
+#     H3_TAG_CLOSE = '</h3>'
+#     P_TAG = '<p>'
+#     P_TAG_CLOSE = '</p>'
+#
+#     def test_home_user_not_authorized(self):
+#         client = Client()
+#
+#         login = "Login</a>"
+#         response = client.get(reverse("core:index"))
+#         content = str(response.content)
+#         welcome = "{}Welcome!{}".format(self.H1_TAG, self.H1_TAG_CLOSE)
+#         text = "{}Please login to view content{}".format(self.H2_TAG, self.H2_TAG_CLOSE)
+#         self.assertEqual(response.status_code, 200)
+#         self.assertEqual(welcome in content, True)
+#         self.assertEqual(text in content, True)
+#         self.assertEqual(login in content, True)
+#
+#     def test_home_user_is_authorized(self):
+#         client = Client()
+#
+#         user = CoreTests.create_test_user()
+#         client.force_login(user=user)
+#         response = client.get(reverse("core:index"))
+#         content = str(response.content)
+#         welcome = "{}Welcome, {}!{}".format(self.H1_TAG, user.username, self.H1_TAG_CLOSE)
+#         text = 'href="/user/%s"' % user.id
+#         self.assertEqual(response.status_code, 200)
+#         self.assertEqual(welcome in content, True)
+#         self.assertEqual(text in content, True)
+#         logged_as = "Logged as: %s" % user.username
+#         self.assertEqual(logged_as in content, True)
+#         logout = "Logout"
+#         self.assertEqual(logout in content, True)
+#
+#     def test_users(self):
+#         client = Client()
+#
+#         users = User.objects.all()
+#         response = client.get(reverse("core:users"))
+#         content = str(response.content)
+#         header = "{}Users{}".format(self.H2_TAG, self.H2_TAG_CLOSE)
+#         self.assertEqual(response.status_code, 200)
+#         self.assertEqual(header in content, True)
+#         for user in users:
+#             self.assertEqual(user.username in content, True)
+#
+#     def test_user_pk1(self):
+#         client = Client()
+#
+#         user = CoreTests.create_test_user()
+#         client.force_login(user=user)
+#         response = client.get(reverse("core:user", args=(user.id,)))
+#         content = str(response.content)
+#         user_header = "%s%s%s" % (self.H2_TAG, user.username, self.H2_TAG_CLOSE)
+#         user_email = "%sEmail: %s%s" % (self.P_TAG, user.email, self.P_TAG_CLOSE)
+#         self.assertEqual(response.status_code, 200)
+#         self.assertEqual(user_header in content, True, "%s is not having %s" % (user_header, user.username))
+#         self.assertEqual(user_email in content, True, "%s is not having %s" % (user_email, user.email))
+#
+#     def test_user_pk_not_authorized(self):
+#         client = Client()
+#
+#         response = client.get(reverse("core:user", args=(1,)))
+#         self.assertEqual(response.status_code, 302)
