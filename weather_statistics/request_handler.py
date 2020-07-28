2ed19e815b1e0e4f4057aca430172a57685ff046
tpokora <pokora.tomek@gmail.com>
2020-07-28 10:43:57 +0200

    #25 Handle request

--- weather_statistics/request_handler.py
+++ weather_statistics/request_handler.py
@@ -1,17 +1,21 @@
 from collections import namedtuple
 
+from django.utils.datastructures import MultiValueDictKeyError
+
 
 def handle_request(request):
     ForecastRequest = namedtuple('ForecastRequest', 'latitude longitude period')
     Period = namedtuple('Period', 'start end')
 
     req_data = (request.GET['latitude'], request.GET['longitude'],
-                Period(none_when_empty(request.GET['start']), none_when_empty(request.GET['end'])))
+                Period(none_when_empty(request, 'start'), none_when_empty(request, 'end')))
     req = ForecastRequest._make(req_data)
     return req
 
 
-def none_when_empty(value):
-    if value is None:
+def none_when_empty(request, field):
+    try:
+        value = request.GET[field]
+        return value
+    except MultiValueDictKeyError:
         return None
-    return value
