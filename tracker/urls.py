3e2b38c3d27a92ef96f901b46ca5369633ed8093
tpokora <pokora.tomek@gmail.com>
2020-07-30 18:01:11 +0200

    #26 get records by tracker name

--- tracker/urls.py
+++ tracker/urls.py
@@ -6,7 +6,7 @@ from tracker.views import TrackerList, RecordList, RecordListByTrackerName
 urlpatterns = [
     path('trackers/', TrackerList.as_view()),
     path('records/', RecordList.as_view()),
-    path('records/<tracker_name>', RecordListByTrackerName.as_view()),
+    path('records/<tracker_name>/', RecordListByTrackerName.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
