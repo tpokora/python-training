f699468b5e5ffb17de6982c768488aa31c66b629
tpokora <pokora.tomek@gmail.com>
2020-08-20 22:36:02 +0200

    #26 AlloAny for TrackerList and RecordList

--- tracker/views.py
+++ tracker/views.py
@@ -1,15 +1,19 @@
 # Create your views here.
-from rest_framework import generics, viewsets
+from rest_framework import viewsets
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import AllowAny
 
 from tracker.models import Track, Record
 from tracker.serializers import TrackSerializer, RecordSerializer
 
 
+@permission_classes((AllowAny, ))
 class TrackerList(viewsets.ModelViewSet):
     queryset = Track.objects.all()
     serializer_class = TrackSerializer
 
 
+@permission_classes((AllowAny, ))
 class RecordList(viewsets.ModelViewSet):
     queryset = Record.objects.all()
     serializer_class = RecordSerializer
