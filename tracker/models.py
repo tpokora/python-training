dc7c5ca95cffcf7727d0499fd9e70ab4a650ace3
tpokora <pokora.tomek@gmail.com>
2020-08-29 20:32:14 +0200

    #26 Unique name for tracker

--- tracker/models.py
+++ tracker/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Track(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30, unique=True)
     unit = models.CharField(max_length=30)
     description = models.CharField(max_length=300)
 
